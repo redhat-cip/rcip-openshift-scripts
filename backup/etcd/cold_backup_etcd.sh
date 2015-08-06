6f72006c943427e75987603c138210a0f1adcfe2
Guillaume Coré <fridim@onfi.re>
2015-08-06 15:00:40 +0200

    fix cp

--- backup/etcd/cold_backup_etcd.sh
+++ backup/etcd/cold_backup_etcd.sh
@@ -6,8 +6,8 @@ ORIG=$(cd $(dirname $0); pwd)
 eval $(${ORIG}/get_env.py)
 
 
+mkdir -p "${ORIG}/cold_backup"
 backupdir="${ORIG}/cold_backup/$(date +%Y%m%d%H%M).etcd"
-mkdir -p $backupdir
 
 systemctl stop openshift-master
 cp -r $etcd_storage $backupdir
