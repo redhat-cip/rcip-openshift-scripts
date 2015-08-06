918ccf633614ea38b2c0126922fcb1456045039d
Guillaume Coré <fridim@onfi.re>
2015-08-06 15:53:08 +0200

    purge cold_backup as well

--- backup/etcd/purge.sh
+++ backup/etcd/purge.sh
@@ -6,6 +6,8 @@ KEEP=20
 
 ORIG=$(cd $(dirname $0); pwd)
 
-ls -d $ORIG/*.etcd | head -n "-${KEEP}" | while read i; do
-  [ ! -z "$i" ] && [ -d $i ] && [[ $i =~ .etcd ]] && rm -rf $i
+for d in . cold_backup; do
+    ls -d $ORIG/$d/*.etcd | head -n "-${KEEP}" | while read i; do
+      [ ! -z "$i" ] && [ -d $i ] && [[ $i =~ .etcd ]] && rm -rf $i
+    done
 done
