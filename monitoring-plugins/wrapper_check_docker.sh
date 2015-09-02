58704917aeefbcda11f03a3f6227aa677f583cd3
Guillaume Coré <fridim@onfi.re>
2015-09-02 21:57:03 +0200

    fix typo

--- monitoring-plugins/wrapper_check_docker.sh
+++ monitoring-plugins/wrapper_check_docker.sh
@@ -2,7 +2,7 @@
 
 timeout 5 /opt/check_docker/check_docker --base-url='unix:///var/run/docker.sock' --crit-data-space=90 --warn-data-space=85 --crit-meta-space=90 --warn-meta-space=85
 R=$?
-if [ "$?" = '124' ]; then
+if [ "$R" = '124' ]; then
   echo "CRITICAL docker timeout"
   exit 2
 fi
