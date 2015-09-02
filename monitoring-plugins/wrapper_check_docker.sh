6616e9450f0707fbe73c3de27bc24f2ff9932e62
Guillaume Coré <fridim@onfi.re>
2015-09-02 16:29:46 +0200

    add check_docker wrapper (for timeout)

---
+++ monitoring-plugins/wrapper_check_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+timeout 5 /opt/check_docker/check_docker --base-url='unix:///var/run/docker.sock' --crit-data-space=90 --warn-data-space=85 --crit-meta-space=90 --warn-meta-space=85
+R=0
+if [ "" = '124' ]; then
+  echo "CRITICAL docker timeout"
+  exit 2
+fi
+
+exit 
