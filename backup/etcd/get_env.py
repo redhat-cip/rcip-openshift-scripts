39113665d2743ec53e058a80b63c6c852baae255
Guillaume Coré <fridim@onfi.re>
2015-10-06 15:42:00 +0200

    backup/etcd/get_env.py: support new multi-master conf

--- backup/etcd/get_env.py
+++ backup/etcd/get_env.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import yaml
+import re
 basedir='/etc/openshift/master/'
 
 with open(basedir + 'master-config.yaml', 'r') as stream:
@@ -10,4 +11,11 @@ with open(basedir + 'master-config.yaml', 'r') as stream:
     print('export etcd_certFile=' + basedir + clientconf['certFile'])
     print('export etcd_keyFile=' + basedir + clientconf['keyFile'])
     print('export etcd_url=' + clientconf['urls'][0])
-    print('export etcd_storage=' + conf['etcdConfig']['storageDirectory'])
+    try:
+        stor = conf['etcdConfig']['storageDirectory']
+    except:
+        for line in open('/etc/etcd/etcd.conf', 'r'):
+            if re.search('ETCD_DATA_DIR', line):
+                stor = line.replace('ETCD_DATA_DIR=', '').rstrip()
+                break
+    print('export etcd_storage=%s' % stor)
