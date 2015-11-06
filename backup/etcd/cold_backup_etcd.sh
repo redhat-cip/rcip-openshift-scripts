7d9f35c9493ae1cd5fa9f6c78fe5fc4b48e2edb3
Guillaume Coré <fridim@onfi.re>
2015-11-06 10:22:18 +0100

    etcd cold backup: don't stop openshift-master if in etcd-cluster mode
       two scenarios:
         - etcd in cluster mode outside openshift  (stop etcd service)
         - etcd shipped withing openshift          (stop openshift-master service)

--- backup/etcd/cold_backup_etcd.sh
+++ backup/etcd/cold_backup_etcd.sh
@@ -11,12 +11,16 @@ eval $(${ORIG}/get_env.py)
 mkdir -p "${BACKUPDIR}/cold_backup"
 colddir="${BACKUPDIR}/cold_backup/$(date +%Y%m%d%H%M).etcd"
 
-systemctl stop openshift-master
 if [ "$etcd_outside_openshift" = "yes" ]; then
 	systemctl stop etcd
+else
+	systemctl stop openshift-master
 fi
+
 cp -r $etcd_storage $colddir
+
 if [ "$etcd_outside_openshift" = "yes" ]; then
 	systemctl start etcd
+else
+	systemctl start openshift-master
 fi
-systemctl start openshift-master
