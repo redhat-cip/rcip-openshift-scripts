046b3dd6bfadcec7135bdf1a9cf317c81b0803e1
Guillaume Coré <gucore@redhat.com>
2015-12-17 17:22:56 +0100

    backup etcd purge.sh: make KEEP variable  an env variable
    with this commit, number of backups to keep can be defined
    with KEEP env variable, no need to edit the script.

--- backup/etcd/purge.sh
+++ backup/etcd/purge.sh
@@ -4,7 +4,7 @@ set -e
 BACKUPDIR=${BACKUPDIR:-"/opt/backup/etcd"}
 
 # minimum number of backup to keep
-KEEP=10
+KEEP=${KEEP:-"10"}
 
 for d in hot cold_backup; do
     ls -d $BACKUPDIR/$d/*.etcd | head -n "-${KEEP}" | while read i; do
