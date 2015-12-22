267aa559f9a323dfe55b637123ba416f2532bfe4
gaelL <gael.lambert@enovance.com>
2015-12-22 15:01:27 +0100

    configs/backup_configs.sh: Rename BACKUP_DIR to BACKUPDIR

--- backup/configs/backup_configs.sh
+++ backup/configs/backup_configs.sh
@@ -5,11 +5,11 @@
 # Modification in this git, each time you call this script
 
 # env variables you may want to change
-BACKUP_DIR=${BACKUP_DIR:-"/opt/backups/$HOSTNAME/configs"}
+BACKUPDIR=${BACKUPDIR:-"/opt/backups/$HOSTNAME/configs"}
 FILE_TO_BACKUP=${FILE_TO_BACKUP:-"/etc/ansible /etc/origin"}
 
 function git_exec {
-    git --work-tree=$BACKUP_DIR  --git-dir=$BACKUP_DIR/.git $1
+    git --work-tree=$BACKUPDIR  --git-dir=$BACKUPDIR/.git $1
 }
 
 
@@ -17,21 +17,21 @@ function git_exec {
 if [[ "$1" != "--go" ]]; then
     echo 'Infos:'
     echo '    /!\ This script should be improve later'
-    echo '    This script will create an empty local git repository in BACKUP_DIR. And copy / commit'
+    echo '    This script will create an empty local git repository in BACKUPDIR. And copy / commit'
     echo '    Modification in this git, each time you call this script'
     echo 'Usage:'
     echo "    $0 --go"
     exit 1
 fi
 
-if ! [[ -d  $BACKUP_DIR ]]; then
-    echo "Create and init the backup dir $BACKUP_DIR ..."
-    mkdir -p $BACKUP_DIR
+if ! [[ -d  $BACKUPDIR ]]; then
+    echo "Create and init the backup dir $BACKUPDIR ..."
+    mkdir -p $BACKUPDIR
     git_exec "init"
 fi
 
 echo "Backup files ..."
-rsync -av --delete --exclude="/.git" $FILE_TO_BACKUP $BACKUP_DIR/
+rsync -av --delete --exclude="/.git" $FILE_TO_BACKUP $BACKUPDIR/
 echo "Add files in git ..."
 git_exec "add -A"
 git_exec 'commit -a -m "'$(date +"%Y-%m-%d_%H:%M:%S")'"'
